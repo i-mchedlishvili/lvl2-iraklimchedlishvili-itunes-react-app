1831ada0b062e2249aea95a152dcb51484f3d5d4
i-mchedlishvili <imchedlishvili32@gmail.com>
2021-06-23 18:11:48 +0400

    itunes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import SearchBar from './Search';
 
 function App() {
+
+  const [searchWord, setSearchWord] = useState('eminem');
+  const [Artist, setArtist] = useState(null);
+  const [view, setView] = useState((<div>no data received</div>));
+
+  useEffect(() => {
+    fetch(`https://itunes.apple.com/search?term=${searchWord}`)
+      .then((response) => response.json())
+      .then(data => {
+        console.log(data)
+        const mapi = data.results.map(track => {
+          return {
+            artistName: track.artistName,
+            trackName: track.trackName,
+            collectionName: track.collectionName,
+            link: track.collectionViewUrl
+          }
+        });
+        setArtist(mapi);
+      });
+
+  }, [searchWord]);
+
+  useEffect(() => {
+    if (!!Artist) {
+      const temp = Artist.map(track => (
+        <ul>
+          <li className="artist-name">{track.artistName}</li>
+          <li className="music-name">{track.trackName}</li>
+          <li className="album-name">{track.collectionName}</li>
+          <a href={"link"} target="_blank"> <li className="linki">Link</li> </a>
+        </ul>
+      ));
+      setView(temp);
+    }
+  }, [Artist])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchBar searchh={(word) => { setSearchWord(word) }} />
+      {view}
     </div>
   );
 }
